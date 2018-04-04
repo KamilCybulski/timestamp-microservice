0c851751324bf2319bfb20fbe0faaf9db1babe0c
Kamyl <kamillcybulski@gmail.com>
2018-04-04 10:02:52 +0200

    Remove unnecessary files

--- test/timestamp_microservice_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TimestampMicroserviceWeb.PageControllerTest do
-  use TimestampMicroserviceWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
