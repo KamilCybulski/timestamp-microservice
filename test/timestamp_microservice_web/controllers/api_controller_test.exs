b2e0915e67ce15c356ffc5ea0835670f2d3cd353
Kamil <kamil.cybulski@10clouds.com>
2018-04-18 11:24:16 +0200

    Refactor tests

--- test/timestamp_microservice_web/controllers/api_controller_test.exs
+++ test/timestamp_microservice_web/controllers/api_controller_test.exs
@@ -1,33 +1,83 @@
 defmodule TimestampMicroserviceWeb.ApiControllerTest do
   use TimestampMicroserviceWeb.ConnCase
 
-  test "API input 2005-06-12", %{conn: conn} do
-    conn = get conn, api_path(conn, :index, "2005-06-12")
-    assert json_response(conn, 200) == %{"timestamp" => 1118534400, "natural" => "2005-06-12"}
+  test "API handles valid natural dates", %{conn: conn} do
+    conn1 = get conn, api_path(conn, :index, "1415-07-06")
+    assert json_response(conn1, 200) == %{
+      "timestamp" => -17498073600,
+      "natural" => "1415-07-06"
+    }
+
+    conn2 = get conn, api_path(conn, :index, "1988-04-17")
+    assert json_response(conn2, 200) == %{
+      "timestamp" => 577238400,
+      "natural" => "1988-04-17"
+    }
+
+    conn3 = get conn, api_path(conn, :index, "2066-12-26")
+    assert json_response(conn3, 200) == %{
+      "timestamp" => 3060547200,
+      "natural" => "2066-12-26"
+    }
   end
 
-  test "API input 1988-04-17", %{conn: conn} do
-    conn = get conn, api_path(conn, :index, "1988-04-17")
-    assert json_response(conn, 200) == %{"timestamp" => 577238400, "natural" => "1988-04-17"}
+  test "API handles invalid natural dates", %{conn: conn} do
+    conn1 = get conn, api_path(conn, :index, "2012-16-16")
+    assert json_response(conn1, 200) == %{
+      "error" => true,
+      "reason" => "Invalid date"
+    }
+
+    conn2 = get conn, api_path(conn, :index, "1st March 2012")
+    assert json_response(conn2, 200) == %{
+      "error" => true,
+      "reason" => "Invalid input format"
+    }
+
+    conn3 = get conn, api_path(conn, :index, "qwerty")
+    assert json_response(conn3, 200) == %{
+      "error" => true,
+      "reason" => "Invalid input format"
+    }
   end
 
-  test "API input 2066-12-26", %{conn: conn} do
-    conn = get conn, api_path(conn, :index, "2066-12-26")
-    assert json_response(conn, 200) == %{"timestamp" => 3060547200, "natural" => "2066-12-26"}
-  end
+  test "API handles valid timestamp", %{conn: conn} do
+    conn1 = get conn, api_path(conn, :index, -62167219200)
+    assert json_response(conn1, 200) == %{
+      "timestamp" => -62167219200,
+      "natural" => "0000-01-01"
+    }
 
-  test "GET input 1522950532" do
-    conn = get conn, api_path(conn, :index, "1522950532")
-    assert json_response(conn, 200) == %{"timestamp" => 1522950532, "natural" => "2018-04-05"}
-  end
+    conn2 = get conn, api_path(conn, :index, 0)
+    assert json_response(conn2, 200) == %{
+      "timestamp" => 0,
+      "natural" => "1970-01-01"
+    }
 
-  test "GET input 1582966632" do
-    conn = get conn, api_path(conn, :index, "1582966632")
-    assert json_response(conn, 200) == %{"timestamp" => 1582966632, "natural" => "2020-02-29"}
-  end
+    conn3 = get conn, api_path(conn, :index, 924042898)
+    assert json_response(conn3, 200) == %{
+      "timestamp" => 924042898,
+      "natural" => "1999-04-13"
+    }
 
-  test "GET input 982966632" do
-    conn = get conn, api_path(conn, :index, "982966632")
-    assert json_response(conn, 200) == %{"timestamp" => 982966632, "natural" => "2001-02-23"}
+    conn4 = get conn, api_path(conn, :index, 2524042992)
+    assert json_response(conn4, 200) == %{
+      "timestamp" => 2524042992,
+      "natural" => "2049-12-25"
+    }
+  end
+    
+  test "API handles invalid timestamp", %{conn: conn} do
+    conn1 = get conn, api_path(conn, :index, -62167219201)
+    assert json_response(conn1, 200) == %{
+      "error" => true,
+      "reason" => "Invalid unix time"
+    }
+
+    conn2 = get conn, api_path(conn, :index, 999999999999999999)
+    assert json_response(conn2, 200) == %{
+      "error" => true,
+      "reason" => "Invalid unix time"
+    }
   end
 end
